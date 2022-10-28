4901a6d82245a16da8eb3242481ca0e5bf9a80c4
Gabriel Santana <gabriellima36716@gmail.com>
2022-10-28 20:46:35 -0300

    criando mais rotas com o metodo get para buscar dados no mongodb

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,15 @@
-const userService = require("../services/user.service")
+const userService = require("../services/user.service");
+const mongoose = require("mongoose");
 
 const create = async (req, res) => {
-    const {name, userName, email, password, avatar, background} = req.body;
+    const { name, userName, email, password, avatar, background } = req.body;
 
     if (!name || !userName || !email || !password || !avatar || !background) {
-        res.status(400).send({"message": "è preciso preemcher todos os campos"})
+        res.status(400).send({ "message": "è preciso preemcher todos os campos" })
     }
 
-    const user = await userService.create(req.body);
-    if (!user){
+    const user = await userService.createService(req.body);
+    if (!user) {
         return res.status(400).send({ message: "erro ao criar usuário" });
     }
 
@@ -25,4 +26,29 @@ const create = async (req, res) => {
     })
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
+
+    if (users.length === 0) {
+        return res.status(400).send({ message: "não há usuários cadastrados" })
+    }
+
+    res.send(users)
+}
+
+const findById = async (req, res) => {
+    const id = req.params.id
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({ message: "ID de usuário inválido" })
+    }
+
+    const user = await userService.findByIdService(id)
+
+    if (!user) {
+        return res.status(400).send({ message: "Usuário não encontrado" })
+    }
+
+    res.send(user);
+}
+
+module.exports = { create, findAll, findById };
