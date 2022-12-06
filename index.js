57b474fad536101a8458b6b6d161cc5757e8c98b
Gabriel Santana <gabriellima36716@gmail.com>
2022-12-05 21:14:29 -0300

    documentando com swagger

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv"
 import userRoute from "./src/routes/user.route.js"
 import authRoute from "./src/routes/auth.route.js"
 import postRoute from "./src/routes/post.route.js"
+import swaggerRoute from "./src/routes/swagger.route.js"
 
 dotenv.config()
 
@@ -17,5 +18,7 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/post", postRoute);
+app.use("/doc", swaggerRoute)
+
 
 app.listen(port, () => console.log(`Servidor funcionando na porta: ${port}`));
