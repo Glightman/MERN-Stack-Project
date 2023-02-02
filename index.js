6c5966bee981fd50ac8149398b42c86c75fd5320
Gabriel Santana <gabriellima36716@gmail.com>
2023-02-02 15:19:10 -0300

    adicionando o cors para liberação do consumo

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import express from "express"
 import connectDatabase from "./src/database/db.js"
 import dotenv from "dotenv"
@@ -7,6 +6,12 @@ import userRoute from "./src/routes/user.route.js"
 import authRoute from "./src/routes/auth.route.js"
 import postRoute from "./src/routes/post.route.js"
 import swaggerRoute from "./src/routes/swagger.route.cjs"
+import cors from 'cors';
+
+const alloedOrigins = ["http://localhost:3000"]
+const corsOptions = {
+    origin: alloedOrigins,
+}
 
 dotenv.config()
 
@@ -15,6 +20,7 @@ const app = express();
 
 connectDatabase()
 app.use(express.json());
+app.use(cors(corsOptions))
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/post", postRoute);
--- spa-brakingnews/src/pages/Home/Home.jsx
+++ spa-brakingnews/src/pages/Home/Home.jsx
@@ -1,6 +1,16 @@
 import { Navbar } from "../../assets/components/Navbar/Navbar";
+import { useState, useEffect } from "react";
 
-export default function Home(){
+
+export default function Home() {
+
+    const [news, setNews] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/post').then(response => response.json()).then(data => console.log(data.results)).catch((err) => console.log(err))
+            
+    }, [])
+
     return (
     <>
         <Navbar />
